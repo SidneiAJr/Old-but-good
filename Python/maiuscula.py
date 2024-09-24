ec9e67273fa01f36168c84654c3974bc12ffa1e4
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-24 20:35:23 -0300

    Create maiuscula.py

---
+++ Python/maiuscula.py
@@ -0,0 +1,9 @@
+nome= input("Qual é seu Nome?")
+idade = float(input("Qual sua idade"))
+cidade = input("Qual e sua cidade")
+sobrenome = nome
+
+print(nome.strip())
+print(nome.upper())
+print(nome[0])
+
