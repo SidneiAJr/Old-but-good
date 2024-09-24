200e740cd92a9c4212b2c202e31efcde0b894bce
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-24 13:43:09 -0300

    Create operacao.rb

---
+++ Ruby/operacao.rb
@@ -0,0 +1,17 @@
+
+print 'Digite um numero: '
+v1 = gets.chomp.to_i
+print 'Digite um numero: '
+v2 = gets.chomp.to_i
+print 'Digite um numero: '
+v3 = gets.chomp.to_i
+soma = v1+v2+v3
+div = v1+v2/v3
+subt= v1-v2-v3
+mult= v1*v2*v3
+
+
+puts "A soma é: #{soma}"
+puts "A div é: #{div}"
+puts "A subt é: #{subt}"
+puts "A mult é: #{mult}"
