cc42ceac1ce2c0bfa822605ab897b96eeb1260ef
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-26 19:29:16 -0300

    Create calcpiscina.rb

---
+++ Ruby/calcpiscina.rb
@@ -0,0 +1,15 @@
+
+print 'Digite os Litros em uma piscina:'
+n1 = gets.chomp.to_i
+print 'Horas para encher o objeto'
+n2 = gets.chomp.to_i
+res = n1/n2
+res2 = res/60
+
+
+
+
+puts "O total de Litros*por horas sera: #{res}"
+puts "O total de horas será: #{res2}"
+
+
