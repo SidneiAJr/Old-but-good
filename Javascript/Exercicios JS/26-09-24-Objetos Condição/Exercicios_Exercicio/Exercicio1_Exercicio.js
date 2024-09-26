80b374153993fee4bd38363894956e737ca9b543
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-26 18:25:38 -0300

    Add files via upload

---
+++ Javascript/Exercicios JS/26-09-24-Objetos Condição/Exercicios_Exercicio/Exercicio1_Exercicio.js
@@ -0,0 +1,20 @@
+const filme = {
+	nome: "Auto da Compadecida", 
+	ano: 2000, 
+	elenco: [
+		"Matheus Nachtergaele", "Selton Mello", "Denise Fraga", 
+		"Virginia Cavendish"
+		], 
+	transmissoesHoje: [
+		{canal: "Telecine", horario: "21h"}, 
+		{canal: "Canal Brasil", horario: "19h"}, 
+		{canal: "Globo", horario: "14h"}
+		]
+}
+
+console.log(filme.elenco[0])
+console.log(filme.elenco[filme.elenco.length - 1])
+console.log(filme.transmissoesHoje[2])
+//vai ser impresso na posição 0 que o começo do array elenco, será impresso Matheus Nachtergaele.
+//No array elenco vai ser diminuido um caracter.
+//No ultimo será impresso o filme na posição 2 canal: "Globo", horario: "14h".
