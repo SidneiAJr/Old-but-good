987e361d2cc0ff6b8f7a73eaf24c6cd882e0be85
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-27 11:37:03 -0300

    Create compand.js

---
+++ JS/compand.js
@@ -0,0 +1,9 @@
+let pessoa = prompt("Voce concluiu o ensino medio: ")
+let pessoa2 = prompt("Quantos anos voce tem?: ")
+let pessoa3 = prompt("Voce cursa outra faculdade?:")
+
+if(pessoa==="sim"&&pessoa>=18&&pessoa3==="nao"){
+    console.log("Voce Pode estudar!")
+}else{
+console.log("Voce não pode estudar na faculdade ainda!")
+}
