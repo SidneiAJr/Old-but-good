81d3c5e9dbb7479117a6bd037ab492c7af26a10e
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-29 20:56:52 -0300

    Create op.f

---
+++ Fortran/op.f
@@ -0,0 +1,18 @@
+program Numeros
+
+   real :: v1, v2, resultado,res2,res3,res4,res5
+
+   v1 = 12
+   v2 = 15
+   resultado = v1 + v2
+   res2 = v1*v2
+   res3 = v1-v2
+   res4 = v2/v1
+   res5 = v2*v1/v2
+   print *, 'O Resultado é ', resultado
+   print *, 'O Resultado é ', res2
+   print *, 'O Resultado é ', res3
+   print *, 'O Resultado é ', res4
+   print *, 'O Resultado é ', res5
+
+end program Numeros
