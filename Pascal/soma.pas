f7a1af4cbae706de793b20db664ef1b215490cb4
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-09-29 19:52:41 -0300

    Create soma.pas

---
+++ Pascal/soma.pas
@@ -0,0 +1,21 @@
+program teste;
+
+uses crt;
+
+var
+v1,v2,v3,res: integer;
+
+Begin
+clrscr;
+Write('Por favor insira sua nota:');
+Readln(v1);
+Write('Por favor insira sua nota:');
+Readln(v2);
+Write('Por favor insira sua nota:');
+Readln(v3);
+
+Writeln('Por favor insira sua nota:',res);
+res:=v1+v2+v3;
+
+ 
+end.
