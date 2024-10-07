594e44969605b7f3060b763f7deb4c6ddc243c79
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-07 14:56:29 -0300

    Create Exercicio1.cs

---
+++ C#/Exercicio1.cs
@@ -0,0 +1,32 @@
+//comentario em um linha //
+// Comentario de mais de uma linha//
+using System;
+
+public class Exercicio1
+{
+    public static void Main(string[] args)
+    {
+        int n1=15;
+        int n2 = 30;
+        int n3 = 40;
+        int n4 = 50;
+        int soma ;
+        int soma2;
+        int soma3;
+        
+        Console.WriteLine ("================Bem vindo==============");
+      Console.WriteLine("O Numero e:{0}",n1);
+      Console.WriteLine("O Numero e:{0}",n2);
+      Console.WriteLine("O Numero e:{0}",n3);
+      Console.WriteLine("O Numero e:{0}",n4);
+      soma = n1 + n2;
+      soma2 = n2+ n3;
+      soma3 = n3+n4 ;
+      Console.WriteLine("A soma e:{0}",soma);
+      Console.WriteLine("A soma e:{0}",soma2);
+      Console.WriteLine("A soma e:{0}",soma3);
+      
+
+    }
+}
+
