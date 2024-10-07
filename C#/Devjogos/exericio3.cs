74c0308928c64d86377d919082129bd494f94d56
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-07 17:09:40 -0300

    Create exericio3.cs

---
+++ C#/Devjogos/exericio3.cs
@@ -0,0 +1,20 @@
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("================Bem vindo==============");
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+
+        Console.WriteLine(num1>5);
+        Console.WriteLine(num1<5);
+        Console.WriteLine(num1 == num2);
+        Console.WriteLine(num1 != num2);
+        
+    }
+}
+
