e8265868a49b5e3a42fb242bdb364161775cc939
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-07 16:47:46 -0300

    Update Exercicio2.2.cs

--- C#/Devjogos/Exercicio2.2.cs
+++ C#/Devjogos/Exercicio2.2.cs
@@ -1,1 +1,43 @@
 
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        
+        int num1=2;
+        int num2=4;
+        int num3=20;
+        int num4=40;
+        int num5=100;
+        int num6=5;
+        int num7=5;
+        int num8=5;
+        int num9=5;
+        int num10=5;
+        int testandopar;
+        int testandopar2;
+        int testandopar3;
+        int testandopar4;
+        int testandopar5;
+
+        testandopar=num1%2;
+        testandopar2=num2%2;
+        testandopar3=num3%2;
+        testandopar4=num4%2;
+        testandopar5=num5%2;
+        Console.WriteLine(testandopar);
+        Console.WriteLine(testandopar2);
+        Console.WriteLine("O Resto da divisão dele por 2 tem que igual{0}",testandopar3);
+        Console.WriteLine("O Resto da divisão dele por 2 tem que igual{0}",testandopar4);
+        Console.WriteLine("O Resto da divisão dele por 2 tem que igual{0}",testandopar5);
+
+
+
+
+
+        
+    }
+}
+
