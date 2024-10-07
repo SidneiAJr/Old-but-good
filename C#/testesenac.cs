909c1cad6e85ae6ffbc027aecdcde154cab5bccd
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-07 15:31:50 -0300

    Create testesenac.cs

---
+++ C#/testesenac.cs
@@ -0,0 +1,27 @@
+//comentario em um linha //
+// Comentario de mais de uma linha//
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+       //int inteiro
+        //double numero quebrado
+        //char caractere
+        //string texto
+        //bool verdadeiro falso
+        int idade = 26;
+        double altura = 1.70;
+        char inicial ='S';
+        string nome = "Alberto";
+        bool aprovado = true;
+        
+        Console.WriteLine($"Meu nome e:{nome}e tenho:{altura}Minha Inicial e:{inicial}");
+        
+      
+      
+
+    }
+}
+
