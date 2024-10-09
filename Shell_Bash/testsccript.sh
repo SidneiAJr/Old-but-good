33b436e96748fc1b03372cd4fb577633d133ddcd
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-09 09:59:48 -0300

    Update testsccript.sh

--- Shell_Bash/testsccript.sh
+++ Shell_Bash/testsccript.sh
@@ -1,1 +1,8 @@
 echo "=====Bem vindo ao Sistema======="
+select VAR_CONTROLE in 1 2 3 
+do
+echo "Opcao escolhida $VAR_CONTROLE"
+echo "A opcao seleciona abre o HTOP tem certeza?"
+break;
+done
+echo "================================"
