6f1194b1bb4474e0d5b8d9a59e6d92909032b024
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-10 14:05:54 -0300

    Update foremc.cs

--- C#/Devjogos/foremc.cs
+++ C#/Devjogos/foremc.cs
@@ -1,1 +1,30 @@
+//Loops São estruturas de repeticao
+
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        
+
+
+        {
+         int voltas = 0;
+          //Loop While(Enquanto)
+          while(voltas<10){
+          Console.WriteLine($"Voce esta na volta{voltas}");
+          }
+          
+
+
+        }
+       
+
+       
+        
+    }
+}
+
 
