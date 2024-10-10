f5c0898c67806e29f684a0ff369a9d1f8327599a
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-10 16:21:04 -0300

    Create arraysenac.cs

---
+++ C#/Devjogos/arraysenac.cs
@@ -0,0 +1,20 @@
+using System;
+namespace Teste
+{
+    class Program
+    {
+        static void Main()
+        {
+        int [] arrayts = new int[5]{1,2,3,4,5};
+        
+        
+        Console.WriteLine("O Arrey e:{0}",arrayts[0]);
+        Console.WriteLine("O Arrey e:{0}",arrayts[1]);
+        Console.WriteLine("O Arrey e:{0}",arrayts[2]);
+        Console.WriteLine("O Arrey e:{0}",arrayts[3]);
+        Console.WriteLine("O Arrey e:{0}",arrayts[4]);
+        
+        }
+        
+    }
+}
