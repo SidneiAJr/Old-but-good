11f36905f0bba4594571f500b604e66972b4a552
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-10 14:51:17 -0300

    Update Exercicio5.cs

--- C#/Devjogos/Exercicio5.cs
+++ C#/Devjogos/Exercicio5.cs
@@ -1,1 +1,26 @@
-
+using System;
+namespace Teste
+{
+    class Program
+    {
+        static void Main()
+        {
+        int num=10;
+        
+        while(num>0){
+        if(num==0){
+            Console.Write("Boom");
+        }else if(num %2 ==0){
+           Console.Write("TIC");  
+        }else{
+          Console.Write("TAC");    
+        }
+       num--;
+       
+        }
+        
+        
+        }
+        
+    }
+}
