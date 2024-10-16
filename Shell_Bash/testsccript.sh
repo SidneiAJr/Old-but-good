2ee7057103a2478b1a0a28e83e0815d460f90863
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-16 20:31:04 -0300

    Delete Shell_Bash directory

--- Shell_Bash/testsccript.sh
+++
@@ -1,8 +0,0 @@
-echo "=====Bem vindo ao Sistema======="
-select VAR_CONTROLE in 1 2 3 
-do
-echo "Opcao escolhida $VAR_CONTROLE"
-echo "A opcao seleciona abre o HTOP tem certeza?"
-break;
-done
-echo "================================"
