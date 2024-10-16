78f1d3892e937cf8d7b9b6de07810faa4f0a9985
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-16 20:38:23 -0300

    Update mediapond.php

--- PHP/mediapond.php
+++ PHP/mediapond.php
@@ -1,19 +1,12 @@
 <?php 
-	
-// Simple example prompting the user for their name 
-$nome = readline("Por favor coloque seu nome:  "); 
-$nota1  = (int)readline("Insira a nota1");
-$nota2 = (int)readline("Insira a nota2");
+
+$nota1  = (int)readline("Insira a nota1: ");
+$nota2 = (int)readline("Insira a nota2: ");
 $resultado = $nota1+$nota2;
 
 
-echo "O Valor de sua media é: $resultado";
+echo "O Valor de sua media é:$resultado";
 
-if ($nome !== "") { 
-	echo "Ola, $nome! Bem vindo  "; 
-} else { 
-	echo "Por favor insira um Nome valido  "; 
-} 
 if ($resultado>5 ) { 
 	echo "Aluno Aprovado!"; 
 } else { 
