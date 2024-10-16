8aeb604fabedb264d9a64f4a20cd07f69314f05e
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-16 20:54:09 -0300

    Update inputsomauser.php

--- PHP/inputsomauser.php
+++ PHP/inputsomauser.php
@@ -3,14 +3,17 @@
 $nota1  = (int)readline("Insira a nota1: ");
 $nota2 = (int)readline("Insira a nota2: ");
 $resultado = $nota1+$nota2;
+$resultado2 = $nota1*$nota2;
+$resultado3 = $nota1-$nota2;
+$resultado4 = $nota1/$nota2;
 
 
-echo "O Valor de sua media é:$resultado";
+echo "O Valor de sua media é:$resultado\n";
+echo "O Valor de sua media é:$resultado2\n";
+echo "O Valor de sua media é:$resultado3\n";
+echo "O Valor de sua media é:$resultado4\n";
 
-if ($resultado>5 ) { 
-	echo "Aluno Aprovado!"; 
-} else { 
-	echo "Aluno Reprovado!"; 
-} 
+
+
 
 ?>
