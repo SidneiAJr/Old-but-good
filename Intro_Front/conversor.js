082ab8e625513441e7896281ba6eae032476fb1d
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-16 10:44:05 -0300

    Create conversor.js

---
+++ Intro_Front/conversor.js
@@ -0,0 +1,15 @@
+// Cria a função para converter
+function converter(){
+const valor = document.getElementById("valor").value
+const moeda = document.getElementById("moeda").value
+const taxas = {
+    "USD": 0.17,
+    "EUR": 0.16,
+    "GBP": 0.13,
+    "JPY": 0.03,
+    "CNY": 0.79,
+    "Gold": 0.0291,
+}
+
+}
+
