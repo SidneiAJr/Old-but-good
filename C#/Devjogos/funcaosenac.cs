823d6b3a73cccb16758478039b54418c668f87fe
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-17 14:20:05 -0300

    Update funcaosenac.cs

--- C#/Devjogos/funcaosenac.cs
+++ C#/Devjogos/funcaosenac.cs
@@ -3,14 +3,12 @@ namespace Teste
 {
     class Teste
     {
-        public void calculo(){
-        string Nome = "Albertao";
-        double altura = 1.72;
-        Console.WriteLine ($"Seu Nome e {Nome} e voce tem {altura}");
+        public void calculo(string Nome = "Albertao",double altura = 1.72, int idade = 26){
+        Console.WriteLine ($"Seu Nome e {Nome} e voce tem  {altura} e tenho de idade {idade}");
         }
         static void Main(string[]args){
             Teste meuObj = new Teste();
-            meuObj.calculo();
+            meuObj.calculo("Albertao",1.72,26);
         }
 
     }
