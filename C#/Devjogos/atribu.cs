a616642cac3b61bb0a2fb0dbcac705d12aa97eb7
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-17 15:15:04 -0300

    Update atribu.cs

--- C#/Devjogos/atribu.cs
+++ C#/Devjogos/atribu.cs
@@ -1,6 +1,7 @@
 using System;
     class personagem
     {
+
        //Atributos atributos são variaives  que minha classe usa
        // Constructor e uma função/metodo definido como os objetos
        // Metodos sao função que representam o que objeto dessa classe pode fazer
@@ -11,10 +12,29 @@ using System;
        public double velataque;
 
        public void atacar(){
-         Console.WriteLine($"{nome}Ataca com {forca} Pontos de forca,e tem {velataque}Velocidade de ataque base, O % de roubo de vida é{roubovida}")
+         Console.WriteLine($"{nome}Ataca com {forca} Pontos de forca,e tem {velataque}Velocidade de ataque base, O % de roubo de vida é{roubovida}");
        }
        public void Andar(){
-        Console.WriteLine($"{nome}está Andando.")
+        Console.WriteLine($"{nome}está Andando.");
+       }
+       public personagem(string nomepersonagem,int forcaInicial,int VidaInicial, double velataque2,double roubo){
+          nome = nomepersonagem;
+          forca = forcaInicial;
+          vida = VidaInicial;
+          velataque = velataque2;
+          roubovida = roubo;
        }
            
-        }
+}
+
+class Jogo
+{
+            static void Main(string[] arg){
+                //Criar um personagem
+                personagem personagem1 = new personagem("Queen",20,1,100,0.75);
+                personagem1.atacar();
+                personagem1.Andar();
+
+
+            }
+}    
