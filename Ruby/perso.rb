5e0fb283c72ebb39bd2410d5043f8836a8e09b60
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-19 12:00:07 -0300

    Create perso.rb

---
+++ Ruby/perso.rb
@@ -0,0 +1,9 @@
+print 'Insira o nome do personagem: '
+nome = gets.chomp
+
+roubodevida=10 
+vida=20
+armadura=30
+velataque=30
+
+puts "Bem vindo #{nome} seus atributos são roubo de vida #{roubodevida} vida #{vida} armadura #{armadura} e tem velocidade de ataque #{velataque}"
