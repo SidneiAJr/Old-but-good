2ad6bd5734a3b8162c23c92898360a62b8326ef8
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-19 19:22:46 -0300

    Create foremc.c

---
+++ C/foremc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main () {
+   printf("Loop em C!\n ");
+   int n1=0.0;
+   int n2=0.0;
+    printf("Digite um numero para iniciar a contagem: ");
+   scanf("%d",&n1);
+   for(int i=0; n1>=6; i++){
+    printf("%d\n",n2);
+    n2 = n1++;
+   }
+  
+}
