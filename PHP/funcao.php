82ba39a391c6031c8500bead2686629b2bbe995e
Albertaodasmassa <albertaodasmassa@gmail.com>
2024-10-21 18:35:13 -0300

    Update funcao.php

--- PHP/funcao.php
+++ PHP/funcao.php
@@ -4,13 +4,13 @@ $num1 = (int)readline("Insira um numero: ");
 $num2 = (int)readline("Insira um numero: ");
 $resultado = $num1+$num2;
 
-function voidFunction($resultado): void
+function cad($resultado): void
 {
 	print "O valor é: $resultado\n";
 	return;
 }
 
-voidFunction($resultado); 
+cad($resultado); 
 
 
 
