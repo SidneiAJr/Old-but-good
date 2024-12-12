420e5825548f79294382caa4f28686a0fabf6ebb
Sid <albertaodasmassa@gmail.com>
2024-12-12 14:25:37 -0300

    Update Menu.bat

--- Bats/Menu.bat
+++ Bats/Menu.bat
@@ -10,10 +10,10 @@ echo 2. Abrir Firefox
 echo 3. Abrir ferramenta de captura
 echo 4. Abrir painel de controle
 echo 5. Abrir Chrome
-echo 6. Abrir ferramenta do dxdiag
-echo 7. Sair
-echo.
-set /p opcao=Escolha uma opcao (1 a 10): 
+echo 6. Abrir ferramenta do Dxdiag
+echo 7. Abrir Gerenciador de Tarefas
+echo 8. Sair
+set /p opcao=Escolha uma opcao (1 a 8): 
 
 if "%opcao%"=="1" goto abrir_notepad
 if "%opcao%"=="2" goto abrir_firefox
@@ -21,7 +21,8 @@ if "%opcao%"=="3" goto abrir_ferrawindowscapture
 if "%opcao%" =="4" goto abrir_control
 if "%opcao%" =="5" goto abrir_chrome
 if "%opcao%" =="6" goto abrir_dxdiag
-if "%opcao%"=="7" exit
+if "%opcao%" =="7" goto abrir_gertarefas
+if "%opcao%"=="8" exit
 :abrir_notepad
 start notepad.exe
 goto MENU
@@ -40,4 +41,8 @@ goto MENU
 :abrir_dxdiag
 start dxdiag.exe
 goto MENU
+:abrir_gertarefas
+start taskmgr.exe
+goto MENU
+
 
