0c1e074b8d2b8929d719bf9b7739f821eb623c13
Sid <albertaodasmassa@gmail.com>
2024-12-15 12:55:17 -0300

    Update sistemabasc.py

--- Python/sistemabasc.py
+++ Python/sistemabasc.py
@@ -1,3 +1,40 @@
+# Definindo algumas funções de exemplo
+def opcao1():
+    insira_info()
+def opcao2():
+    cadastra_info()
+
+def sair():
+    print("Saindo do programa...")
+    exit()
+
+# Criando o menu
+def menu():
+    print("Escolha uma opção:")
+    print("1 - Opção 1")
+    print("2 - Opção 2")
+    print("3 - Sair")
+    
+    # Lê a escolha do usuário
+    escolha = input("Digite o número da opção: ")
+    
+    # Usando um dicionário para simular o switch case
+    #Chama a função simular ao js, porem em py, usa essa imundicia toda, em vez de um switch case simples como no js.
+    #que imundicia HAHAHA
+    switch = {
+        '1': insira_info,
+        '2': cadastra_info,
+        '3': sair
+    }
+    
+    # Chama a função correspondente à escolha ou exibe mensagem de erro
+    func = switch.get(escolha)
+    if func:
+        func()
+    else:
+        print("Opção inválida!")
+
+# Chamando o menu
 def insira_info():
     nome = input("Olá Insira seu nome: ")
     idade = int(input("Oá Insira sua idade: "))
@@ -8,7 +45,7 @@ def insira_info():
         print("Voce pode fazer carteira de motora!")
     else:
        print("Voce não pode fazer carteira de motora!")
-    if ensinomedio == "sim":
+    if ensinomedio == "s":
         print("Voce completou o ensino medio!")
     else:
         print("Voce não completou o ensino medio!")
@@ -17,5 +54,5 @@ def cadastra_info():
     id_abrigo = int(input("Insira o numero do abrigo"))
     array_abrigo = []
     array_abrigo.insert(i)
-cadastra_info()
-insira_info()
+menu()
+
