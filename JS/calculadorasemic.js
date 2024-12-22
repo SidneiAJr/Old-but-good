41f41df0909567316f2749a92e9ad6b7b0e030f4
Sid <albertaodasmassa@gmail.com>
2024-12-21 22:02:13 -0300

    Update calculadorasemic.js

--- JS/calculadorasemic.js
+++ JS/calculadorasemic.js
@@ -1,10 +1,13 @@
-const menu = prompt("Informa a opção 1 ou 2:")
+const menu = prompt("Informa a opção 1 ou 4:")
 switch(menu){
     case '1':
     calc()
     break
     case '2':
     calc2()
+    break
+    case '3':
+    calc3()
     break
     deFault:
     console.log("Opção Invalida")
@@ -27,4 +30,16 @@ const mes = tempo/12
 const calculo = valorIni*(1+juros2)**mes
 console.log("O Valor em R$",calculo)
 return calculo.toFixed(100)
+}
+function calc3(){
+const valorIni= Number(prompt("Favor Informar o Valor Inicial: "))
+const juros = Number(prompt("Favor Informar o juros do cupom: "))
+const tempo = Number(prompt("Favor Informar o Tempo em Anos "))
+const ipcaAtual = Number(prompt("Favor Informe o ipca atual"))
+const juros2 = juros/100
+const ipcaAtual2 = ipcaAtual/100
+const somajuros = juros2+ipcaAtual2
+const calculo = valorIni*(1+somajuros)**tempo
+console.log("O Valor em R$",calculo)
+return calculo.toFixed(100)  
 }
