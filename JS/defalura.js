80f023fef3cd2b809eee14ace036867cc1905ce3
Sid <sidalbertok@gmail.com>
2025-01-15 11:39:04 -0300

    Create defalura.js

---
+++ JS/defalura.js
@@ -0,0 +1,16 @@
+//alert("Bem vindo ao desafio da alura sem prompt")
+//const nome = "lua"
+//const idade = 25
+//const numeroDeVendas = 50
+//const SaldoDisponveil = 1000
+//alert("Erro! Preencha todos os campos")
+
+alert("Bem vindo ao sistema dos guri da alura com prompt")
+let nome2 = prompt("Digite seu nome: ")
+let idade2 = prompt("Digite sua idade: ")
+
+if(idade2>18){
+    alert("Podes Dirigir o auto dos guri")
+}else{
+    alert("Não podes dirigr!")
+}
