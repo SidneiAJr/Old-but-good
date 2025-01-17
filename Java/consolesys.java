f68b74141fa2c2ec279dad4574d94e3bc7863003
Sid <sidalbertok@gmail.com>
2025-01-17 11:11:41 -0300

    Update consolesys.java

--- Java/consolesys.java
+++ Java/consolesys.java
@@ -1,5 +1,4 @@
-
-    public static void calc1(){
+ public static void calc1(){
         Scanner entrada = new Scanner(System.in);
         double valorIni;
         System.out.print("Digite o valor inicial: ");
@@ -29,12 +28,34 @@
         res = (rend*100)/valorApli;
         System.out.print("O juros atual é: "+res);
     }
+    public static void cad(){
+     
+        Scanner reb = new Scanner(System.in);
+        String nome;
+        System.out.print("Digite seu nome: ");
+        nome = reb.nextLine();
+        String sobrenome;
+        System.out.print("Digite seu sobrenome: ");
+        sobrenome = reb.nextLine();
+        String tel;
+        System.out.print("Digite seu Telefone: ");
+        tel = reb.nextLine();
+        String matricula;
+        System.out.print("Digite seu Matricula: ");
+        matricula = reb.nextLine();
+        String salario;
+        System.out.print("Digite seu Salario: ");
+        salario = reb.nextLine();
+        
+        String[] info = {nome , sobrenome,tel,matricula,salario};
+        System.out.println("Olá Bem vindo: " + info[0] + " " + info[1]+ "Telefone: " + info[2]+ "Matricula: "+ info[3]+"Salario R$:"+ info[4]);
+    }
     
     public static void main(String[] args) {
       
     int op;
     Scanner entrada = new Scanner(System.in);
-    System.out.print("Digite uma opção");
+    System.out.print("Digite uma opcao : ");
     op=entrada.nextInt();
     
     switch(op){
@@ -43,6 +64,9 @@
         break;
         case 2:
         juros();
+        break;
+        case 3:
+        cad();
         break;
         default:
         System.out.println("Opção inválida!");
