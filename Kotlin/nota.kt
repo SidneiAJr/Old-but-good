4eae48af98e472d3e869a1cb4e9d3d689a9b0a02
Sid <sidalbertok@gmail.com>
2025-01-22 19:57:57 -0300

    Create nota.kt

---
+++ Kotlin/nota.kt
@@ -0,0 +1,20 @@
+fun main() {
+    
+    print("Insira seu nome")
+    val nome = readLine()
+    print("Insira seu sobrenome")
+    val sobrenome = readLine()
+    println("ola $nome $sobrenome")
+    printl("Insira sua nota")
+    val n1 : Int = readLine()
+    printl("Insira sua nota")
+    val n2 : Int = readLine()
+    printl("Insira sua nota")
+    val n3 : int = readLine()
+    val res = (n1+n2+n3)
+    println("Sua nota $res")
+    
+    
+    
+    
+}
