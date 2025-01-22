5d54d950cec4630df88f9ecc96f0343698401159
Sid <sidalbertok@gmail.com>
2025-01-22 17:35:02 -0300

    Update soma.kt

--- Kotlin/soma.kt
+++ Kotlin/soma.kt
@@ -4,3 +4,19 @@ fun main() {
     val res = n1 +n2
     println("O Resultado e $res")
 }
+
+fun main() {
+    val n1: Int = 5
+    val n2: Int = 5
+    val n3: Int = 5
+    val res = (n1 +n2 + n3)/3
+
+    if(res<=5){
+        println("Aluno foi de vrau")
+    }else{
+        println("Não vai ter vrau")
+    }
+
+    
+    println("O Resultado e $res")
+}
