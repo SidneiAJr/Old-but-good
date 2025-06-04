cc2e030df05d03c2813028f8976f5122794319b5
Sid Jr <sidalbertok@gmail.com>
2025-06-04 20:35:43 -0300

    Update Soma.r

--- R/Soma.r
+++ R/Soma.r
@@ -1,4 +1,10 @@
 num1 <- 5
 num2 <- 5
 resultado <- num1+num2
+resultado2 <- num1-num2
+resultado3 <- num1*num2
+resultado4 <- num1/num2
 print(resultado)
+print(resultado2)
+print(resultado3)
+print(resultado4)
