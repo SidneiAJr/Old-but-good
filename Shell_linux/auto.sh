b2d6bbfec1b191976c756b31f05ee2edb55570f8
Sid Jr <sidalbertok@gmail.com>
2025-08-01 18:54:02 -0300

    Delete Shell_linux directory

--- Shell_linux/auto.sh
+++
@@ -1,6 +0,0 @@
-echo "Automatizador c/ comando"
-sudo apt upgrade -y
-sudo apt update -y
-sudo apt dist-upgrade -y
-sudo apt autoremove -y
-sudo apt clean -y
--- Shell_linux/basic.sh
+++
@@ -1,4 +0,0 @@
-echo "Instalador basico"
-sudo apt install vim -y
-sudo apt install htop -y
-sudo apt upgrade -y
